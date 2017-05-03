cd560cb65acf657a96300620f8d3a3d02a47b040
Jack Taylor <jack@blueinkcode.com>
2017-05-03 16:28:43 +0100

    Added button graphics

--- Cuby/ViewController.swift
+++ Cuby/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var checkInButton: UIButton!
+    
+    @IBOutlet weak var topCheckInButton: UIButton!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotateAnimation.fromValue = 0.0
+        rotateAnimation.toValue = 360.0
+        rotateAnimation.repeatCount = HUGE
+        rotateAnimation.duration = 450
+        
+        checkInButton.layer.add(rotateAnimation, forKey: "myAnimationKey");
+        
+        
+
+        
     }
 
 }
